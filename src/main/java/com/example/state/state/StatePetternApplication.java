043c25bb997a48df9d5ab248fc6961f2a8ad2f42
jamie <jamie@cntt.co.kr>
2020-05-08 12:54:30 +0900

    state pattern init

---
+++ src/main/java/com/example/state/state/StatePetternApplication.java
@@ -0,0 +1,13 @@
+package com.example.state.state;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatePetternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StatePetternApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/state/state/StatePetternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.state.state;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatePetternApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
